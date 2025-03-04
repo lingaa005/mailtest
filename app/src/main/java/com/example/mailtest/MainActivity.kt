27c433b70a7bbfa398c2666a9f5e420bdce3013d
Lingaa <kasilingamtvm@gmail.com>
2025-03-04 16:13:30 +0530

    version 4 working testing in progress

--- app/src/main/java/com/example/mailtest/MainActivity.kt
+++ app/src/main/java/com/example/mailtest/MainActivity.kt
@@ -11,24 +11,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mailtest.model.Email
 import kotlin.concurrent.thread
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var emailAdapter: EmailAdapter
     private val emails = mutableListOf<Email>()
-
     private val handler = Handler(Looper.getMainLooper())
 
     private val fetchEmailsRunnable = object : Runnable {
-        @SuppressLint("NotifyDataSetChanged")
         override fun run() {
             thread {
                 val fetchedEmails = GmailFetcher.fetchEmails()
-                Handler(Looper.getMainLooper()).post {
-                    emails.clear()
-                    emails.addAll(fetchedEmails)
-                    emailAdapter.notifyDataSetChanged()
-                    Toast.makeText(this@MainActivity, "Emails Updated", Toast.LENGTH_SHORT).show()
+
+                runOnUiThread {
+                    updateEmailList(fetchedEmails)
                 }
             }
             handler.postDelayed(this, 2000) // Run again after 2 seconds
@@ -61,5 +56,32 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         handler.removeCallbacks(fetchEmailsRunnable) // Stop auto-fetching when activity is destroyed
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun updateEmailList(newEmails: List<Email>) {
+        val oldSize = emails.size
+        val newSize = newEmails.size
+
+        // Compare and update only the changed parts
+        if (newSize > oldSize) {
+            val addedEmails = newEmails.subList(oldSize, newSize)
+            emails.addAll(addedEmails)
+            emailAdapter.notifyItemRangeInserted(oldSize, addedEmails.size)
+        } else if (newSize < oldSize) {
+            emails.clear()
+            emails.addAll(newEmails)
+            emailAdapter.notifyDataSetChanged() // Only needed if emails are removed
+        } else {
+            // Update only modified emails
+            for (i in newEmails.indices) {
+                if (emails[i] != newEmails[i]) {
+                    emails[i] = newEmails[i]
+                    emailAdapter.notifyItemChanged(i)
+                }
+            }
+        }
+
+        Toast.makeText(this@MainActivity, "Emails Updated", Toast.LENGTH_SHORT).show()
     }
 }
